aba7043679e32368b7e0c4b28549ee53a63b25d4
Adventitious Studios <mebsport@gmail.com>
2019-11-03 01:16:09 -0400

    Ball
    Set up the ball movement and checking if it is still on a platform in the ball controller script

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+
+    bool started;
+    bool gameOver;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        started = false;
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!started)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+                started = true;
+            }
+        }
+
+        Debug.DrawRay(transform.position, Vector3.down, Color.blue);
+
+        if(!Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            gameOver = true;
+            rb.velocity = new Vector3(0, -25f, 0);
+        }
+
+        if(Input.GetMouseButton(0) && !gameOver)
+        {
+            SwitchDirection();
+        }
+    }
+
+    void SwitchDirection()
+    {
+        if(rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+        else if(rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+    }
+}
