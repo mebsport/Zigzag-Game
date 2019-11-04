65677086923562627fc3389bf0a63801d6c3a283
Adventitious Studios <mebsport@gmail.com>
2019-11-03 20:19:10 -0500

    Platform Destruction
    Created script to make platforms fall after the ball rolls over them

---
+++ Assets/Scripts/TriggerChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChecker : MonoBehaviour
+{
+    private void OnTriggerExit(Collider col)
+    {
+        if(col.gameObject.tag == "Ball")
+        {
+            Invoke("FallDown", 0.5f);
+        }
+    }
+
+    void FallDown()
+    {
+        GetComponentInParent<Rigidbody>().useGravity = true;
+
+        Destroy(transform.parent.gameObject, 3);
+    }
+}
