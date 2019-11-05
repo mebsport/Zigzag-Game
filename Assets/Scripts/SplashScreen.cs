dbad24c2598724cc19575345d5108a7f6bf12c9c
Adventitious Studios <mebsport@gmail.com>
2019-11-05 02:24:13 -0500

    UI
    Worked on UI and animations for the game

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SplashScreen : MonoBehaviour
+{
+    public Text splashText;
+
+    IEnumerator Start()
+    {
+        splashText.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("Game");
+    }
+    void FadeIn()
+    {
+        splashText.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+    void FadeOut()
+    {
+        splashText.CrossFadeAlpha(0.0f, 2.4f, false);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public GameObject homePanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void GameStart()
+    {
+        tapText.SetActive(false);
+        homePanel.GetComponent<Animator>().Play("panelUp");
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
